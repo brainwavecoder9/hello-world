04082cd0109caf7c696a0f82033ac9f09e6fabd0
Daniel Ritchie <daniel@brainwavecollective.ai>
2024-09-28 18:51:29 -0600

    Update postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -3,3 +3,4 @@
 # after all system packages and programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
+echo 'workbench ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/workbench
